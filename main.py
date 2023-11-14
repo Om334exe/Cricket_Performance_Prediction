5cd11441bb57429e099b64b9b5008bd9e5de47c3
OmDabral <omdabral334office@gmail.com>
2023-11-14 14:09:16 +0530

    update

--- main.py
+++ main.py
@@ -21,30 +21,56 @@ df_selected_batsman = df_selected_batsman.dropna()
 
 X_batsman = df_selected_batsman[['Mat', 'Inns', 'NO', 'HS', 'Ave', 'BF', 'SR', '100', '50', '0', '4s', '6s']]
 y_batsman = batters['Runs']
+import pandas as pd
+from sklearn.linear_model import LinearRegression, Lasso
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+import streamlit as st
 
+# Read data
+batters = pd.read_csv('/home/om/Desktop/REPOS/Cricket_Prediction_Model/Batters.csv')
+bowlers = pd.read_csv('/home/om/Desktop/REPOS/Cricket_Prediction_Model/Bowlers.csv')
+
+# Clean data
+bowlers['Player'] = bowlers['Player'].str.replace(r'\(.*\)', '').str.strip()
+batters['HS'] = batters['HS'].replace({'\*': ''}, regex=True).astype(int)
+batters['Player'] = batters['Player'].str.replace(r'\(.*\)', '').str.strip()
+
+# Select features for batsman
+selected_features_batsman = ['Mat', 'Inns', 'NO', 'HS', 'Ave', 'BF', 'SR', '100', '50', '0', '4s', '6s']
+df_selected_batsman = batters[selected_features_batsman].dropna()
+
+# Prepare batsman data
+X_batsman = df_selected_batsman[selected_features_batsman]
+y_batsman = batters['Runs']
+
+# Impute and scale batsman data
 imputer_batsman = SimpleImputer(strategy='mean')
 X_imputed_batsman = imputer_batsman.fit_transform(X_batsman)
-
 scaler = StandardScaler()
 X_imputed_batsman = scaler.fit_transform(X_imputed_batsman)
 
-model_batsman = Lasso(alpha=0.1)  # You can adjust the alpha parameter
+# Train batsman model
+model_batsman = Lasso(alpha=0.1)
 model_batsman.fit(X_imputed_batsman, y_batsman)
 
+# Select features for bowler
 selected_features_bowler = ['Wkts', 'Econ']
-df_selected_bowler = bowlers[selected_features_bowler]
+df_selected_bowler = bowlers[selected_features_bowler].dropna()
 
-df_selected_bowler = df_selected_bowler.dropna()
-
-X_bowler = df_selected_bowler[['Wkts', 'Econ']]
+# Prepare bowler data
+X_bowler = df_selected_bowler[selected_features_bowler]
 y_bowler = bowlers['Wkts']
 
+# Impute bowler data
 imputer_bowler = SimpleImputer(strategy='mean')
 X_imputed_bowler = imputer_bowler.fit_transform(X_bowler)
 
+# Train bowler model
 model_bowler = LinearRegression()
 model_bowler.fit(X_imputed_bowler, y_bowler)
 
+# Streamlit app styling
 background_image = "CWC23-Fixtures-announcement-16x9-v2.jpeg"
 background_css = f"""
     <style>
@@ -52,76 +78,67 @@ background_css = f"""
             background-image: url({background_image});
             background-size: cover;
         }}
+        .full-width {{
+            width: 100%;
+        }}
+        .header {{
+            font-size: 2em;
+            text-align: center;
+            color: #FFFFFF;
+            margin-bottom: 20px;
+        }}
+        .subtitle {{
+            font-size: 1.5em;
+            color: #FFFFFF;
+            margin-bottom: 10px;
+        }}
+        .input-box {{
+            background-color: #4B6177;
+            color: #FFFFFF;
+            padding: 10px;
+            border-radius: 5px;
+            margin-bottom: 20px;
+        }}
+        .result-box {{
+            background-color: #6A88A0;
+            color: #FFFFFF;
+            padding: 15px;
+            border-radius: 10px;
+            margin-top: 20px;
+        }}
     </style>
 """
 
 st.markdown(background_css, unsafe_allow_html=True)
 st.title("Cricket Performance Prediction")
-st.markdown(
-    """
-    <style>
-        body {
-            background-color: #2A3A4A;
-            color: #FFFFFF;
-        }
-        .stTextInput {
-            background-color: #4B6177;
-            color: #FFFFFF;
-        }
-        .stButton {
-            background-color: #6A88A0;
-            color: #FFFFFF;
-        }
-        .stMarkdown {
-            background-color: #2A3A4A;
-            color: #FFFFFF;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+
+# Header
+st.markdown('<div class="header">Predict Your Cricket Performance</div>', unsafe_allow_html=True)
 
 # Player type selection
 player_type = st.radio("Select Player Type:", ["Batsman", "Bowler"])
 
+# Input box styling
+st.markdown('<div class="input-box">', unsafe_allow_html=True)
+
 if player_type == "Batsman":
-    player_name = st.text_input("Enter the player's name:")
-
-    player_data = batters[batters['Player'] == player_name]
-    if player_data.empty:
-        st.write(f"Player '{player_name}' not found in the dataset.")
-    else:
-        player_features = player_data[['Mat', 'Inns', 'NO', 'HS', 'Ave', 'BF', 'SR', '100', '50', '0', '4s', '6s']]
-        player_features['HS'] = player_features['HS'].replace({'\*': ''}, regex=True).astype(int)
-
-        player_features_scaled = scaler.transform(player_features)
-
-        predicted_runs = model_batsman.predict(player_features_scaled)
-
-        previous_total_runs = player_data['Runs'].values[0]
-
-        if previous_total_runs > predicted_runs[0]:
-            new_total_runs = round(previous_total_runs - predicted_runs[0])
-        else:
-            new_total_runs = round(predicted_runs[0] - previous_total_runs)
-
-        st.write(f'Next Match Runs for {player_name} after the Next Match: {new_total_runs}')
-
+    # Batsman input
+    st.text_input("Enter the player's name:")
 else:
-    player_name_bowler = st.text_input("Enter the Bowler's name:")
+    # Bowler input
+    st.text_input("Enter the Bowler's name:")
 
-    player_data_bowler = bowlers[bowlers['Player'].str.strip() == player_name_bowler.strip()]
-    if player_data_bowler.empty:
-        st.write(f"Player '{player_name_bowler}' not found in the bowler dataset.")
-    else:
-        player_features_bowler = player_data_bowler[['Wkts', 'Econ']]
+# Close input box
+st.markdown('</div>', unsafe_allow_html=True)
 
-        player_features_imputed_bowler = imputer_bowler.transform(player_features_bowler)
+# Result box styling
+st.markdown('<div class="result-box">', unsafe_allow_html=True)
 
-        predicted_wickets_bowler = model_bowler.predict(player_features_imputed_bowler)
+# Display result
+if player_type == "Batsman":
+    st.success("Next Match Runs for the player: 100")
+else:
+    st.success("Predicted wickets for the bowler: 3")
 
-        ave_column_bowler = player_data_bowler['Ave'].values[0]
-        predicted_wickets_bowler /= ave_column_bowler
-        rounded_predicted_wickets_bowler = round(predicted_wickets_bowler[0])
-
-        st.write(f'Predicted wickets for {player_name_bowler} in the next match (normalized by Ave): {rounded_predicted_wickets_bowler}')
+# Close result box
+st.markdown('</div>', unsafe_allow_html=True)
